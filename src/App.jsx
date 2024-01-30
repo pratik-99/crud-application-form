0325e3684900a3416757a6853af939d7c8664971
pratikpoojary <pratik.poojary@somaiya.edu>
2024-01-30 23:03:27 +0530

    Inital commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='card-box'>
+
+    <form>
+      <input placeholder='Firstname' ></input>
+      <input placeholder='Lastname' ></input>
+      <input placeholder='Age' ></input>
+      <input placeholder='Phnoe.no' ></input>
+    </form>
+    </div>
+  );
+}
+
+export default App;
