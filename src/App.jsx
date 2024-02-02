ca4f3bc8573573e69de81bb2a4049d0c0f064b0e
pratikpoojary <pratik.poojary@somaiya.edu>
2024-02-02 23:52:25 +0530

    state maintenance

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
-import logo from './logo.svg';
+import React, {useState} from "react"
 import './App.css';
 
 function App() {
+
+  const [formData,setFormData]=useState(
+    {
+      firstname:"", 
+      lastname:"", 
+      age:"",
+      phone:""
+    }
+    )
+  function onHandleChange(event){
+    setFormData(
+      prev=>{
+      return{...prev, [event.target.name]:[event.target.value]}
+    })
+  }
+  console.log(formData)
   return (
     <div className='card-box'>
 
     <form>
-      <input placeholder='Firstname' ></input>
-      <input placeholder='Lastname' ></input>
-      <input placeholder='Age' ></input>
-      <input placeholder='Phnoe.no' ></input>
+      <input placeholder='Firstname' onChange={onHandleChange} name="firstname"></input>
+      <input placeholder='Lastname' onChange={onHandleChange} name="lastname"></input>
+      <input placeholder='Age' onChange={onHandleChange} name="age"></input>
+      <input placeholder='Phnoe.no' onChange={onHandleChange} name="phone"></input>
     </form>
     </div>
   );
