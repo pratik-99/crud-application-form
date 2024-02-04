38e888785841cd7acc685eeef97cbd433ba51839
pratikpoojary <pratik.poojary@somaiya.edu>
2024-02-04 22:36:39 +0530

    table data state added

--- src/App.jsx
+++ src/App.jsx
@@ -11,22 +11,31 @@ function App() {
       phone:""
     }
     )
+
+  const [tableData, setTableData]=useState([])  
   function onHandleChange(event){
     setFormData(
       prev=>{
       return{...prev, [event.target.name]:[event.target.value]}
     })
   }
+  function handleSubmit(e){
+    e.preventDefault()
+
+  }
   console.log(formData)
   return (
     <div className='card-box'>
 
-    <form>
+    <form onSubmit={handleSubmit}>
       <input placeholder='Firstname' onChange={onHandleChange} name="firstname"></input>
       <input placeholder='Lastname' onChange={onHandleChange} name="lastname"></input>
       <input placeholder='Age' onChange={onHandleChange} name="age"></input>
       <input placeholder='Phnoe.no' onChange={onHandleChange} name="phone"></input>
+      <button>submit</button>
     </form>
+
+
     </div>
   );
 }
